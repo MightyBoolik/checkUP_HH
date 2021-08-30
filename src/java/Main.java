634099cda510f169b966ec708eab76fb50286944
MightyBoolik <bkuchaev@gmail.com>
2021-08-30 23:00:55 +0500

    Метод на поиск числа в массиве

--- src/java/Main.java
+++ src/java/Main.java
@@ -1,74 +1,42 @@
-/*public class Main {
-
-    public static void main(String[] args) {
-        int n = 4, m = 6;
-        int cn1 = 199;
-        int cn2 = 453;
-        int cn3 = 220;
-        int cn4 = 601;
-        int[] mas1 = new int[]{};
-        for (int i = 0; i < mas1.length; i++) {
-            mas1[i] = i + 1;
-            //Arrays.sort(mas1);
-            System.out.println(mas1[i]);
-
-        }
-        int index = linearSearch(new int[]{}, 200);
-        print(200, index);
-    }
-
-    public static int linearSearch(int mas1[], int elementToSearch) {
-
-        for (int index = 0; index < mas1.length; index++) {
-            if (mas1[index] == elementToSearch)
-                return index;
-        }
-        return -1;
-
-    }
-
-    public static void print(int elementToSearch, int index) {
-        if (index == -1) {
-            System.out.println(elementToSearch + " not found.");
-        } else {
-            System.out.println(elementToSearch + " found at index: " + index);
-        }
-
-    }
-
-}*/
-
-
 import java.util.Scanner;
 
-public class Main
-{
-
+public class Main {
     public static void main(String[] args) {
-        int[] nums = new int[100];
-
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = (int) ((Math.random() * 100) + 1);
-            System.out.print(nums[i] + " , ");
-        }
-        System.out.println();
-        Scanner input = new Scanner(System.in);
-        int num;
-        System.out.println("What number would you like to search for?");
-        num = input.nextInt();
-        boolean found = false;
-        int i; // create this
-        for (i = 0; i < nums.length; i++)  // and remove int from for loop
-        {
-            if (num == nums[i]) {
-                found = true;
-                break;
-            }
-        }
-        if (found) {
-            System.out.println("That number was found at index " + i);
+        int n = 4, m = 6;
+        int cn1 = 13000;
+        int cn2 = 30000;
+        int cn3 = 22000;
+        int cn4 = 60000;
+        int[] sumOfDeposites = {cn1, cn2, cn3, cn4};
+        int sum = 0;
+        for (int i : sumOfDeposites)
+            sum += i;
+        System.out.println("Deposit for bonus is " + sum);
+        Scanner scnr = new Scanner(System.in);
+        System.out.println("Введите значения cn1 и cn2");
+        int a = scnr.nextInt();
+        int b = scnr.nextInt();
+        if (a < b) // вычисляем минимум 2 чисел
+            System.out.printf("Between %d and %d, minimum is %d %n", a, b, a);
+        int possibleMin = Math.min(a, b);
+        System.out.printf("Minimum value of %d and %d using Math.max() is %d %n", a, b, possibleMin);
+        System.out.println("Введите значения cn3 и cn4");
+        int x = scnr.nextInt();
+        int y = scnr.nextInt();
+        if (x < y) {
+            System.out.printf("Between %d and %d, Minimum Number is %d %n", x, y, x);
         } else {
-            System.out.println("That number was not found.");
+            System.out.printf("Between %d and %d, Minimum is %d %n", x, y, y);
         }
-    }}
+        int min1 = Math.min(x, y);
+        System.out.printf("Minimum value of %d and %d using Math.min() is %d %n", x, y, min1);
+        if (possibleMin < min1)
+            System.out.printf("Between %d and %d, minimum is %d %n", possibleMin, min1, possibleMin);
+        int maxPossibleValue = Math.min(possibleMin, min1);
+        int bonusForEmployee = sum / maxPossibleValue;
+        if (bonusForEmployee > m) {
+            System.out.println("Maximum bonus for employers can be " + possibleMin);
+        }
+    }
+}
 
---
+++ src/java/Main1.java
@@ -0,0 +1,74 @@
+/*public class Main {
+
+    public static void main(String[] args) {
+        int n = 4, m = 6;
+        int cn1 = 199;
+        int cn2 = 453;
+        int cn3 = 220;
+        int cn4 = 601;
+        int[] mas1 = new int[]{};
+        for (int i = 0; i < mas1.length; i++) {
+            mas1[i] = i + 1;
+            //Arrays.sort(mas1);
+            System.out.println(mas1[i]);
+
+        }
+        int index = linearSearch(new int[]{}, 200);
+        print(200, index);
+    }
+
+    public static int linearSearch(int mas1[], int elementToSearch) {
+
+        for (int index = 0; index < mas1.length; index++) {
+            if (mas1[index] == elementToSearch)
+                return index;
+        }
+        return -1;
+
+    }
+
+    public static void print(int elementToSearch, int index) {
+        if (index == -1) {
+            System.out.println(elementToSearch + " not found.");
+        } else {
+            System.out.println(elementToSearch + " found at index: " + index);
+        }
+
+    }
+
+}*/
+
+
+import java.util.Scanner;
+
+public class Main1
+{
+
+    public static void main(String[] args) {
+        int[] nums = new int[100];
+
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = (int) ((Math.random() * 100) + 1);
+            System.out.print(nums[i] + " , ");
+        }
+        System.out.println();
+        Scanner input = new Scanner(System.in);
+        int num;
+        System.out.println("What number would you like to search for?");
+        num = input.nextInt();
+        boolean found = false;
+        int i; // create this
+        for (i = 0; i < nums.length; i++)  // and remove int from for loop
+        {
+            if (num == nums[i]) {
+                found = true;
+                break;
+            }
+        }
+        if (found) {
+            System.out.println("That number was found at index " + i);
+        } else {
+            System.out.println("That number was not found.");
+        }
+    }}
+
