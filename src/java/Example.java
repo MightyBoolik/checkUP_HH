ba73f5a330d0e04208541b2f3bb0875246be377d
MightyBoolik <bkuchaev@gmail.com>
2021-08-31 16:27:22 +0500

    Метод на поиск числа в массиве

--- src/java/Example.java
+++ src/java/Example.java
@@ -12,33 +12,38 @@ public class Example {
         for (int i : sumOfDeposites)
             sum += i;
         System.out.println("Total deposit for bonus is " + sum);
-        System.out.println("Введите колличество счетов и менеджеров через пробел");
-        int n = scnr.nextInt();
-        int m = scnr.nextInt();
-        System.out.println("Введите суммы на корпоративных счетах (один счёт - одна строка)");
-        int a = scnr.nextInt();
-        int b = scnr.nextInt();
-        int x = scnr.nextInt();
-        int y = scnr.nextInt();
-        if (a < b) // вычисляем минимум 2 чисел
-            System.out.printf("Between %d and %d, minimum is %d %n", a, b, a);
-        int possibleMin = Math.min(a, b);
-        System.out.printf("Minimum value of %d and %d using Math.max() is %d %n", a, b, possibleMin);
-        if (x < y) {
-            System.out.printf("Between %d and %d, Minimum Number is %d %n", x, y, x);
-        } else {
-            System.out.printf("Between %d and %d, Minimum is %d %n", x, y, y);
-        }
-        int min1 = Math.min(x, y);
-        System.out.printf("Minimum value of %d and %d using Math.min() is %d %n", x, y, min1);
-        if (possibleMin < min1)
-            System.out.printf("Between %d and %d, minimum is %d %n", possibleMin, min1, possibleMin);
-        int maxPossibleValue = Math.min(possibleMin, min1);
-        int bonusForEmployee = sum / maxPossibleValue;
-        if (bonusForEmployee > m) {
-            System.out.println("Maximum bonus for employers can be " + possibleMin);
+        try {
+            System.out.println("Введите колличество счетов и менеджеров через пробел");
+            int n = scnr.nextInt();
+            int m = scnr.nextInt();
+            System.out.println("Введите суммы на корпоративных счетах (один счёт - одна строка)");
+            int a = scnr.nextInt();
+            int b = scnr.nextInt();
+            int x = scnr.nextInt();
+            int y = scnr.nextInt();
+            if (b > a) // вычисляем минимум 2 чисел
+                System.out.printf("Between %d and %d, minimum is %d %n", a, b, a);
+            int possibleMin = Math.min(a, b);
+            System.out.printf("Minimum value of %d and %d using Math.min() is %d %n", a, b, possibleMin);
+            if (y > x) {
+                System.out.printf("Between %d and %d, Minimum Number is %d %n", x, y, x);
+            } else {
+                System.out.printf("Between %d and %d, Minimum is %d %n", x, y, y);
+            }
+            int min1 = Math.min(x, y);
+            System.out.printf("Minimum value of %d and %d using Math.min() is %d %n", x, y, min1);
+            if (possibleMin > min1)
+                System.out.printf("Between %d and %d, minimum is %d %n", possibleMin, min1, possibleMin);
+            int maxPossibleValue = Math.min(possibleMin, min1);
+            int bonusForEmployee = sum / maxPossibleValue;
+            if (bonusForEmployee > m) {
+                System.out.println("Maximum bonus for employers can be " + possibleMin);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            scnr.close();
         }
     }
-
 }
 
